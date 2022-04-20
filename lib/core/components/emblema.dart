e69611c0bc0043e195a84de7a2034df9d2ef8a8c
Mirjon Ramazonov <mirxan2564@gmail.com>
2022-04-21 03:10:23 +0500

    birinchi page tayyor

---
+++ lib/core/components/emblema.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:stady/core/constants/color_const.dart';
+
+class Emblema extends StatelessWidget {
+  const Emblema({
+    Key? key,
+    this.size = 1,
+  }) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size * 25,
+      width: size * 77,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: size * 24,
+            width: size * 24,
+            decoration: BoxDecoration(
+                color: ColorConst.kWhiteColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(size * 9),
+                )),
+            child: CircleAvatar(
+              backgroundColor: ColorConst.kBlueColor,
+              radius: size * 7.5,
+              child: CircleAvatar(
+                backgroundColor: ColorConst.kWhiteColor,
+                radius: size * 2.4,
+              ),
+            ),
+          ),
+          Text(
+            "Study",
+            style: TextStyle(
+              color: ColorConst.kWhiteColor,
+              fontFamily: "Poppins",
+              fontSize: size * 15,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/core/constants/color_const.dart
+++ lib/core/constants/color_const.dart
@@ -6,4 +6,5 @@ class ColorConst {
   static Color kGreyColor = const Color(0xff64748B);
   static Color kBlueColor = const Color(0xff52B6DF);
   static Color kDarkBlueColor = const Color(0xff4178D4);
+  static Color kButtonColor = const Color(0xff52B6DF);
 }
